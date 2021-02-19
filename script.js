099efeda6888a775db76cd2868582e3c500bc834
Edouard Misset <edouardmisset@gmail.com>
2021-02-19 14:36:53 +0100

    refactoring

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ let secondCheckbox;
 for (let index = 0; index < checkboxes.length; index++) {
   const checkbox = checkboxes[index];
   checkbox.addEventListener("click", (e) => {
-    if (e.shiftKey) {
-      secondCheckbox = index;
-    } else {
-      firstCheckbox = index;
-    }
+    e.shiftKey ? (secondCheckbox = index) : (firstCheckbox = index);
+
+    let firstIndex, secondIndex;
+
     if (firstCheckbox < secondCheckbox) {
-      for (let i = firstCheckbox; i < secondCheckbox; i++) {
-        checkboxes[i].checked = true;
-      }
+      firstIndex = firstCheckbox;
+      secondIndex = secondCheckbox;
     } else {
-      for (let i = secondCheckbox; i < firstCheckbox; i++) {
-        checkboxes[i].checked = true;
-      }
+      firstIndex = secondCheckbox;
+      secondIndex = firstCheckbox;
+    }
+
+    for (let i = firstIndex; i < secondIndex; i++) {
+      checkboxes[i].checked = true;
     }
   });
 }
